7e4085a1f23f7f6c445a5b7d9a300d1a16dfc177
Caoimhe O Donovan <20107569@setu.ie>
2026-05-15 13:48:09 +0100

    Added upcoming movies endpoint

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import { getMovies, getUpcomingMovies } from '../tmdb-api'; 
 
 
 const router = express.Router();
@@ -10,6 +10,11 @@ const router = express.Router();
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
+}));
+
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
 }));
 
 
